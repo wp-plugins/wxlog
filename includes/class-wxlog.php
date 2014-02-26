ccb41d229997be3817cbe06cf5fa7d1905141245
zhangji <zhangji@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-26 05:14:16 +0000

    update by zhangji
    git-svn-id: https://plugins.svn.wordpress.org/wxlog/trunk@865210 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-wxlog.php
+++ includes/class-wxlog.php
@@ -11,6 +11,7 @@ class WL {
 			$upload_dir = wp_upload_dir();
 			$dir = $upload_dir['basedir'].'/wxlog_logs/';
 			if (!is_dir($dir)) @mkdir($dir, 0777);
+			//echo $dir.date("Y-m-d").'.txt';
 			wxlog($dir.date("Y-m-d").'.txt','GET:'.var_export($_GET, TRUE));
 			wxlog($dir.date("Y-m-d").'.txt','HTTP_RAW_POST_DATA:'.var_export(@$GLOBALS["HTTP_RAW_POST_DATA"], TRUE));		
 		}
@@ -22,6 +23,7 @@ class WL {
 		if($_POST["test"]){
 			$postStr = @$_POST["HTTP_RAW_POST_DATA"];
 		}
+		//$postStr = unicode_encode($postStr);
 		if (!empty($postStr) and $this->checkSignature()){
 			$postArray = wxlog_xml_to_array($postStr);
 			$this->wxlog_log_id = $this->insert_wxlog_log($postArray,$postStr,$reply);
@@ -94,17 +96,16 @@ class WL {
 		//黑名单过滤		
 		$wxlog_blacklist_user = get_option( 'wxlog_blacklist_user' );
 		if($wxlog_blacklist_user){
-			$contentStr = get_option( 'wxlog_blacklist_message' );
-			if($contentStr=='custom')
-				$contentStr = get_option( 'wxlog_blacklist_message_custom' );
-			if(in_array($postArray['FromUserName'],explode(',',$wxlog_blacklist_user))){
-				if(empty($contentStr)){
-					$contentStr = "您已经被列入黑名单。";
-				}
-				$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $contentStr);
-				if($resultStr){
-					$wpdb->update( $wpdb->wxlog_log, array( 'reply' => $resultStr,'status' => 2 ), array( 'ID' => $this->wxlog_log_id ), array( '%s','%d' ), array( '%d' ) );
-					exit($resultStr);
+			$contentStr = get_option( 'wxlog_blacklist_message_custom' );
+			if(in_array($postArray['FromUserName'],explode(',',str_replace('，',',',$wxlog_blacklist_user)))){
+				if(!empty($contentStr)){
+					$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $contentStr);
+					if($resultStr){
+						$wpdb->update( $wpdb->wxlog_log, array( 'reply' => $resultStr,'status' => 2 ), array( 'ID' => $this->wxlog_log_id ), array( '%s','%d' ), array( '%d' ) );
+						exit($resultStr);
+					}
+				}else{
+					return '';
 				}
 			}
 		}
@@ -112,18 +113,18 @@ class WL {
 		//调用自定义回复数据
 		$custom_reply_array = $wpdb->get_results( "SELECT ID,keyword FROM {$wpdb->wxlog_custom_reply} where status = 2" );
 		foreach($custom_reply_array as $key=>$value){
-			if(in_array($postArray['Content'],explode(',',$value->keyword))){
+			if(in_array($postArray['Content'],explode(',',str_replace('，',',',$value->keyword)))){
 				$custom_reply = $wpdb->get_row( "SELECT * FROM {$wpdb->wxlog_custom_reply} where ID = {$value->ID}" );
 				break;
 			}
-		}//d($custom_reply);		
+		}//d($custom_reply);
 		if($custom_reply){
 			if($custom_reply->msgtype=='text'){
-				$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $custom_reply->content);
+				$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], stripslashes($custom_reply->content));
 			}elseif($custom_reply->msgtype=='news'){
 				$contentArr = array();
 				$contentArr[0]['title'] =  $custom_reply->title;
-				$contentArr[0]['description'] =  $custom_reply->content;
+				$contentArr[0]['description'] =  stripslashes($custom_reply->content);
 				$contentArr[0]['image_url'] =  $custom_reply->image_url;
 				$contentArr[0]['url'] =  $custom_reply->url;
 				$resultStr = $this->reply_news($postArray['FromUserName'], $postArray['ToUserName'], $contentArr);
@@ -193,37 +194,47 @@ class WL {
 				}
 			}
 		}
-		//是否开启小黄鸡，可以到高级里设置。
-		if(get_option( 'wxlog_simsimi' )){
-			$contentStr = $this->simsimi( $postArray['Content'] );
-			if($contentStr){
-				$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $contentStr);
-				if($resultStr){
-					$wpdb->update( $wpdb->wxlog_log, array( 'reply' => $resultStr,'status' => 2 ), array( 'ID' => $this->wxlog_log_id ), array( '%s','%d' ), array( '%d' ) );
-					exit($resultStr);
-				}
-			}
-		}
 		
-		$contentStr = base64_decode("5qyi6L+O5YWz5rOo5oiR55qE5b6u5L+h5pel5b+X44CCIGh0dHA6Ly93d3cucGhwbG9nLmNvbS93eGxvZw==");
-		$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $contentStr);
+		//默认的回复内容		
+		$contentStr = get_option( 'wxlog_default_content' );
+		if($contentStr){
+			$resultStr = $this->reply_text($postArray['FromUserName'], $postArray['ToUserName'], $contentStr);
+		}
 		if($resultStr){
 			$wpdb->update( $wpdb->wxlog_log, array( 'reply' => $resultStr,'status' => 2 ), array( 'ID' => $this->wxlog_log_id ), array( '%s','%d' ), array( '%d' ) );
 			exit($resultStr);
 		}
     }
 
-	
-
 	//查询数据库
 	public function get_posts($keyword){
 		$post_max = ( $wxlog_post_max = get_option( 'wxlog_post_max' ) ) ? $wxlog_post_max : '5';
-		$query_array = array(
-			's' 					=> $keyword, 
-			'posts_per_page'		=> $post_max, 
-			'post_status' 			=> 'publish', 
-			'ignore_sticky_posts'	=> 1
-		);
+		switch ($keyword) {
+			case get_option( 'wxlog_post_new' ) :
+			 	break;
+			case get_option( 'wxlog_post_hot' ) :
+				if(get_option( 'wxlog_post_hot_field_name' )){
+					$query_array['meta_key'] = 'views';//访问统计的字段名
+					$query_array['orderby'] = 'meta_value_num';
+					$query_array['order'] = 'DESC';
+					break;
+				}
+			case get_option( 'wxlog_post_hot_comment' ) :
+				$query_array['orderby'] = 'comment_count';
+				$query_array['order'] = 'DESC';
+				break;
+			case in_array($keyword,explode(',',str_replace('，',',',get_option( 'wxlog_post_category' )))) :
+				$query_array['cat'] = $keyword;
+			 	break;
+			default :
+				$query_array['s'] = $keyword;
+				break;
+		}//print_r($query_array);
+		
+		$query_array['showposts'] = $post_max;
+		$query_array['posts_per_page'] = $post_max;
+		$query_array['post_status'] = 'publish';
+		$query_array['ignore_sticky_posts'] = 1;
 		$query = new WP_Query($query_array);
 		$contentArr = '';
 		$i = 0;
@@ -243,35 +254,6 @@ class WL {
 			}
 		}
 		return $contentArr;
-	}
-
-
-	//小黄鸡智能机器人接口
-	public function simsimi( $keyword ){
-		if ( $keyword<>'' ){
-			$curlPost = 'para='.$keyword;
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, 'http://www.xiaohuangji.com/ajax.php');
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_NOBODY, true);
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $curlPost);
-			$return_str = curl_exec($curl);
-			$http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-			if ($return_str === false) {
-				return false;//连接失败
-			}
-			if ($http_code !== 200) {
-				return false;//连接失败
-			}		
-			curl_close($curl);
-			if(md5($return_str)=='3f46a0d792a6a4104c11283d4768192d'){
-				//抱歉，小黄鸡还不能理解，求您教我,使用"问...答....."句式(不准带标点~)
-				return false;
-			}
-			return $return_str;
-		}return false;
 	}
 
 	//公众平台绑定验证
@@ -312,7 +294,7 @@ class WL {
 <Content><![CDATA[%s]]></Content>
 <FuncFlag>0</FuncFlag>
 </xml>";
-		return sprintf($textTpl, $contentStr);
+		return sprintf($textTpl, wxlog_emoji(wxlog_qqface($contentStr)));
 	}
 
 	//回复图片消息
--- includes/wxlog-functions.php
+++ includes/wxlog-functions.php
@@ -1,5 +1,70 @@
 <?php
 
+if(!function_exists('wxlog_emoji')){
+	function wxlog_emoji($content,$html='') {
+		/*表情替换*/
+		$f = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
+		$r = "笑脸 开心 大笑 热情 眨眼 色 接吻 亲吻 脸红 露齿笑 满意 戏弄 吐舌 无语 得意 汗 失望 低落 呸 焦虑 担心 震惊 悔恨 眼泪 哭 破涕为笑 晕 恐惧 心烦 生气 睡觉 生病 恶魔 外星人 心 心碎 丘比特 闪烁 星星 叹号 问号 睡着 水滴 音乐 火 便便 强 弱 拳头 胜利 上 下 右 左 第一 强壮 吻 热恋 男孩 女孩 女士 男士 天使 骷髅 红唇 太阳 下雨 多云 雪人 月亮 闪电 海浪 猫 小狗 老鼠 仓鼠 兔子 狗 青蛙 老虎 考拉 熊 猪 牛 野猪 猴子 马 蛇 鸽子 鸡 企鹅 毛虫 章鱼 鱼 鲸鱼 海豚 玫瑰 花 棕榈树 仙人掌 礼盒 南瓜灯 鬼魂 圣诞老人 圣诞树 礼物 铃 庆祝 气球 CD 相机 录像机 电脑 电视 电话 解锁 锁 钥匙 成交 灯泡 邮箱 浴缸 钱 炸弹 手枪 药丸 橄榄球 篮球 足球 棒球 高尔夫 奖杯 入侵者 唱歌 吉他 比基尼 皇冠 雨伞 手提包 口红 戒指 钻石 咖啡 啤酒 干杯 鸡尾酒 汉堡 薯条 意面 寿司 面条 煎蛋 冰激凌 蛋糕 苹果 飞机 火箭 自行车 高铁 警告 旗 男人 女人 O X 版权 注册商标 商标 公交";
+		$i =  '1f604,1f60a,1f603,263a,1f609,1f60d,1f618,1f61a,1f633,1f63c,1f60c,1f61c,1f445,1f612,1f60f,1f613,1f640,1f61e,1f616,1f625,1f630,1f628,1f62b,1f622,1f62d,1f602,1f632,1f631,1f620,1f63e,1f62a,1f637,1f47f,1f47d,2764,1f494,1f498,2728,1f31f,2755,2754,1f4a4,1f4a6,1f3b5,1f525,1f4a9,1f44d,1f44e,1f44a,270c,1f446,1f447,1f449,1f448,261d,1f4aa,1f48f,1f491,1f466,1f467,1f469,1f468,1f47c,1f480,1f48b,2600,2614,2601,26c4,1f319,26a1,1f30a,1f431,1f429,1f42d,1f439,1f430,1f43a,1f438,1f42f,1f428,1f43b,1f437,1f42e,1f417,1f435,1f434,1f40d,1f426,1f414,1f427,1f41b,1f419,1f420,1f433,1f42c,1f339,1f33a,1f334,1f335,1f49d,1f383,1f47b,1f385,1f384,1f381,1f514,1f389,1f388,1f4bf,1f4f7,1f3a5,1f4bb,1f4fa,1f4de,1f513,1f512,1f511,1f528,1f4a1,1f4eb,1f6c0,1f4b2,1f4a3,1f52b,1f48a,1f3c8,1f3c0,26bd,26be,26f3,1f3c6,1f47e,1f3a4,1f3b8,1f459,1f451,1f302,1f45c,1f484,1f48d,1f48e,2615,1f37a,1f37b,1f377,1f354,1f35f,1f35d,1f363,1f35c,1f373,1f366,1f382,1f34f,2708,1f680,1f6b2,1f684,26a0,1f3c1,1f6b9,1f6ba,2b55,274e,a9,ae,2122,1f68c';		
+		$f = explode(',',$f);
+		$r = explode(' ',$r);
+		$i = explode(',',$i);
+		foreach($r as $key=>$val){
+			$rr[] = '<'.$val.'>';
+			$ii[] =  '<span class="emoji emoji'.$i[$key].'"></span>';
+		}
+		if($html){
+			return str_replace($f,$ii,$content);		
+		}else{
+			return str_replace($rr,$f,$content);		
+		}
+	}
+	
+}
+
+
+/**
+ * 判断是否是QQ表情
+ * 
+ * @param content
+ * @return
+ */
+if(!function_exists('wxlog_qqface')){
+	//print_r(wxlog_qqface('[微笑][美女]'));
+	//print_r(wxlog_qqface('/::)/::B','img'));
+	function wxlog_qqface($content,$img='') {
+		/*表情替换*/
+		$f = array('/::)','/::~','/::B','/::|','/:8-)','/::<','/::$','/::X','/::Z','/::\'(','/::-|','/::@','/::P','/::D','/::O','/::(','/::+','/:Cb','/::Q','/::T','/:,@P','/:,@-D','/::d','/:,@o','/::g','/:|-)','/::!','/::L','/::>','/::,@','/:,@f','/::-S','/:?','/:,@x','/:,@@','/::8','/:,@!','/:!!!','/:xx','/:bye','/:wipe','/:dig','/:handclap','/:&-(','/:B-)','/:<@','/:@>','/::-O','/:>-|','/:P-(','/::\'|','/:X-)','/::*','/:@x','/:8*','/:pd','/:<W>','/:beer','/:basketb','/:oo','/:coffee','/:eat','/:pig','/:rose','/:fade','/:showlove','/:heart','/:break','/:cake','/:li','/:bome','/:kn','/:footb','/:ladybug','/:shit','/:moon','/:sun','/:gift','/:hug','/:strong','/:weak','/:share','/:v','/:@)','/:jj','/:@@','/:bad','/:lvu','/:no','/:ok','/:love','/:<L>','/:jump','/:shake','/:<O>','/:circle','/:kotow','/:turn','/:skip','/[]','/:#-0','/[]','/:kiss','/:<&','/:&>');
+		$r = array('微笑','伤心','美女','发呆','墨镜','哭','羞','哑','睡','哭','囧','怒','调皮','笑','惊讶','难过','酷','汗','抓狂','吐','笑','快乐','奇','傲','饿','累','吓','汗','高兴','闲','努力','骂','疑问','秘密','乱','疯','哀','鬼','打击','bye','汗','抠','鼓掌','糟糕','恶搞','什么','什么','累','看','难过','难过','坏','亲','吓','可怜','刀','水果','酒','篮球','乒乓','咖啡','美食','动物','鲜花','枯','唇','爱','分手','生日','电','炸弹','刀','足球','虫','臭','月亮','太阳','礼物','伙伴','赞','差','握手','优','恭','勾','顶','坏','爱','不','好的','爱','吻','跳','怕','尖叫','圈','拜','回头','跳','天使','激动','舞','吻','瑜伽','太极');
+		$i = array();
+		//echo count($r);
+		foreach($r as $key=>$val){
+			$rr[] = '['.$val.']';
+			//$ii[] =  '<img src="https://wx.qq.com/zh_CN/htmledition/images/qqface/'.$key.'.png" />';
+			$ii[] =  '<img src="/wp-content/plugins/wxlog/includes/admin/images/qqface/'.$key.'.png" />';
+		}
+		if($img){
+			return str_replace($f,$ii,$content);		
+		}else{
+			return str_replace($rr,$f,$content);		
+		}
+	}
+
+}
+
+
+if(!function_exists('wxlog_make_signature')){
+	function wxlog_make_signature() {
+        $timestamp = intval(time()); 
+        $nonce = intval($timestamp+3600*24*7); 
+        $token = TOKEN; 
+        $tmpArr = array($token, $timestamp, $nonce);//print_r($tmpArr);
+        sort($tmpArr,SORT_STRING);//print_r($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+  		return array($tmpStr,$timestamp,$nonce);
+	}
+}
 if(!function_exists('wxlog_xml_to_array')){
 	function wxlog_xml_to_array($xml){
 
@@ -295,21 +360,22 @@ if(!function_exists('wxlog_get_xml')){
 	}
 }
 
-function XML_unserialize($xml){
-    $xml_parser = new XML();
-    $data = $xml_parser->parse($xml);
-    $xml_parser->destruct();
-    return $data;
+if(!function_exists('WXLOG_XML_unserialize')){
+	function WXLOG_XML_unserialize($xml){
+		$xml_parser = new WXLOG_XML();
+		$data = $xml_parser->parse($xml);
+		$xml_parser->destruct();
+		return $data;
+	}
 }
-
-class XML{
+class WXLOG_XML{
     var $parser;   #a reference to the XML parser
     var $document; #the entire XML structure built up so far
     var $parent;   #a pointer to the current parent - the parent will be an array
     var $stack;    #a stack of the most recent parent at each nesting level
     var $last_opened_tag; #keeps track of the last tag opened.
 
-    function XML(){
+    function WXLOG_XML(){
         $this->parser = xml_parser_create();
         xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, false);
         xml_set_object($this->parser, $this);
